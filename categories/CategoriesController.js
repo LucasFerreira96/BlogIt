0198479a1dbe6a95f7d41f3190d4468b68a4b0de
LucasFerreira96 <lucas-ferreira@outlook.com>
2021-11-30 19:15:45 -0300

    Instalado Slugify, criado rota para adicionar novas categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,28 @@
+const { application } = require("express");
 const express = require("express");
+const Category = require("./Category");
 const router = express.Router();
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req,res) => {
     res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title
+    if(title != undefined){
+
+      Category.create({
+          title: title,
+          slug: slugify (title)
+      }).then(() => {
+          res.redirect("/");
+      })  
+
+    }else{
+        res.redirect("admin/categories/new")
+    }
+
 });
 
 module.exports = router;
