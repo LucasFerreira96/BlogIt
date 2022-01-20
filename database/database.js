1070f23c9e313797caad116f793e048bc8723744
LucasFerreira96 <lucas-ferreira@outlook.com>
2022-01-19 21:26:58 -0300

    Corrigindo timezone do banco de dados

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 const connection = new Sequelize("BlogItDB", "root", "atxl42mn7pq20", {
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    timezone: "-03:00"
 });
 
 module.exports = connection;
