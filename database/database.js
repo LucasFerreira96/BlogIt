f665049da2e3ee12c311ffa41d4c097d92eca042
Lucas Ferreira <lucas-ferreira@outlook.com>
2022-02-02 20:52:51 -0300

    reinstalado npm

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const connection = new Sequelize("BlogItDB", "root", "atxl42mn7pq20", {
+const connection = new Sequelize("BlogItDB", "root", "@Atxl42mn7pq20", {
     host: "localhost",
     dialect: "mysql",
     timezone: "-03:00"
