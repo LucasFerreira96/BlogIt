0ef617c0f990a8747cf4311912787c03cbd76a76
Lucas Ferreira <lucas-ferreira@outlook.com>
2022-02-06 20:34:31 -0300

    Criado rota para editar artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,25 +4,25 @@ const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
 
-router.get("/admin/articles", (req,res) => {
-    
+router.get("/admin/articles", (req, res) => {
+
     Article.findAll({
-        include: [{model: Category}]
+        include: [{ model: Category }]
     }).then(articles => {
-        res.render("admin/articles/index", {articles: articles});
+        res.render("admin/articles/index", { articles: articles });
 
     })
 });
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
-        res.render("admin/articles/new",{categories: categories});
+        res.render("admin/articles/new", { categories: categories });
     })
 
-    
+
 });
 
-router.post("/articles/save", (req,res) => {
+router.post("/articles/save", (req, res) => {
     var title = req.body.title;
     var body = req.body.body;
     var category = req.body.category;
@@ -35,13 +35,13 @@ router.post("/articles/save", (req,res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     })
-    
+
 });
 
 router.post("/admin/articles/delete", (req, res) => {
     var id = req.body.id;
-    if(id != undefined){
-        if(!isNaN(id)){
+    if (id != undefined) {
+        if (!isNaN(id)) {
 
             Article.destroy({
                 where: {
@@ -51,12 +51,52 @@ router.post("/admin/articles/delete", (req, res) => {
                 res.redirect("/admin/articles");
             });
 
-        }else{//SE ID NÃO FOR UM NUMERO
+        } else {//SE ID NÃO FOR UM NUMERO
             res.redirect("/admin/articles");
         }
-    }else{ //SE ID FOR NULO
+    } else { //SE ID FOR NULO
         res.redirect("/admin/articles");
     }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id
+
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+
+                res.render("admin/articles/edit", { categories: categories, article: article });
+            });
+
+        } else {
+            res.redirect("/admin/articles");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/articles");
+        
+    });
+});
+
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+
+    Article.update({ title: title, body: body, categoryId: category, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/admin/articles");
+        console.log(err);
+    });
+
 });
 
 module.exports = router;
